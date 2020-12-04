1d6afc1c5f17b4f6d2fb3641253f8513ad2cb11c
Joe Reiff <jreiff@spreedly.com>
2020-12-04 17:58:58 -0500

    redo first solution with classes

--- day-4/solve.rb
+++ day-4/solve.rb
@@ -1,6 +1,22 @@
-REQUIRED = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-entries = File.readlines('input.txt').join.split("\n\n").map(&:split)
-parsed = entries.map{|entry| entry.map{|string| string.split(":")}}
+class PassportParser
+  def self.parse(input)
+    entries = File.readlines(input).join.split("\n\n").map(&:split)
+    parsed = entries.map{|entry| entry.map{|string| string.split(":")}}
+  end
+end
 
-p parsed.map { |entry| entry.transpose}.count {|passport| REQUIRED.all? {|field| passport.first.include?(field)}}
+
+class Passport
+  REQUIRED = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+  def initialize(input)
+    @data = Hash[input]
+  end
+
+  def valid?
+    REQUIRED.all? { |field| @data.keys.include?(field) }
+  end
+end
+
+p PassportParser.parse('input.txt').map {|entry| Passport.new(entry)}.count(&:valid?)
 
