dffe4754af2d99dbc876445ec51107f7aa85fab2
Joe Reiff <jreiff@spreedly.com>
2020-12-06 17:22:40 -0500

    complete part 2

--- day-6/solve.rb
+++ day-6/solve.rb
@@ -1,1 +1,9 @@
-p File.read('input.txt').split("\n\n").map{ |group| group.gsub("\n", "").chars.uniq.count }.sum
+groups = File.read('input.txt').split("\n\n")
+
+unique_responses = groups.map{ |group| group.gsub("\n", "").chars.uniq.count }.sum
+
+def find_common_chars(group)
+  group.split.map(&:chars).reduce {|a, b| a & b }
+end
+
+common_responses = groups.map {|group| find_common_chars(group).count }.sum
