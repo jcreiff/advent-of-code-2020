6f5d5ab98dc16c8ca8db5384fb418a6f88f3cee3
Joe Reiff <jreiff@spreedly.com>
2020-12-09 22:09:48 -0500

    completes part 2

--- day-7/solve.rb
+++ day-7/solve.rb
@@ -1,18 +1,21 @@
 class BagMaker
   def self.make_bags(input)
-    colors = File.readlines(input).map {|bag| bag.scan(/(\w+\s\w+)\sbag/).flatten }
-    colors.each { |bag| Bag.new(bag[0], bag[1..-1])}
+    raw = File.readlines(input)
+    colors = raw.map {| bag| bag.scan(/(\w+\s\w+)\sbag/).flatten }
+    numbers = raw.map { |bag| bag.scan(/(\d)/).flatten.map(&:to_i) }
+    colors.zip(numbers).each { |bag| Bag.new(bag.first[0], bag.first[1..-1], bag.last)}
     Bag.all.map { |bag| bag.contents.each { |c| Bag.find(c) << bag.color }} 
   end
 end
 
 class Bag
   @@collection = {}
-  attr_reader :color, :contents, :contained_by
+  attr_reader :color, :contents, :contained_by, :full_contents
 
-  def initialize(color, contents)
+  def initialize(color, contents, numbers)
     @color = color
     @contents = contents.reject {|color| color == "no other" }
+    @full_contents = @contents.zip(numbers).map { |bag| Array.new(bag.last, bag.first) }.flatten
     @contained_by = []
     @@collection[@color] = self
   end
@@ -37,7 +40,16 @@ class Bag
     up_the_chain(next_level, containers.flatten.uniq)
   end
 
+  def down_the_chain(next_level = nil, children = nil)
+    children ||= full_contents
+    next_level ||= full_contents.map { |c| Bag.find(c) }
+    return children.flatten.count if next_level.empty? 
+    children << next_level.map(&:full_contents)
+    next_level = next_level.map(&:full_contents).flatten.map { |c| Bag.find(c) }
+    down_the_chain(next_level, children)
+  end
 end
 
 BagMaker.make_bags('input.txt')
-p Bag.find('shiny gold').up_the_chain
+Bag.find('shiny gold').up_the_chain
+Bag.find('shiny gold').down_the_chain
