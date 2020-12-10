013d1bebca6d6bb5aec36705f564e9f71134ec3d
Joe Reiff <jreiff@spreedly.com>
2020-12-09 23:47:00 -0500

    part 2 complete

--- day-8/solve.rb
+++ day-8/solve.rb
@@ -1,7 +1,6 @@
-instructions = File.readlines('input.txt').map { |line| line.split(' ') }
-
 def find_loop(list, index = 0, total = 0)
   return total if list[index].last == 'complete!'
+  return [total, true] if index == list.length-1
   action, distance = list[index]
   list[index].push('complete!')
   if action == 'jmp'
@@ -13,4 +12,22 @@ def find_loop(list, index = 0, total = 0)
   find_loop(list, index, total)
 end
 
-p find_loop(instructions)
+def instructions
+  File.readlines('input.txt').map { |line| line.split(' ') }
+end
+
+def try_modified(list, i)
+  list[i][0] = (list[i][0] == 'jmp' ? 'nop' : 'jmp')
+  find_loop(list)
+end
+
+def find_success
+  (0...instructions.length).to_a.each do |i|
+    next if instructions[i][0] == 'acc'
+    result, success = try_modified(instructions, i)
+    return result if success
+  end
+end
+
+find_loop(instructions)
+find_success
